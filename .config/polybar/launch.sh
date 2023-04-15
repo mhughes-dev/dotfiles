7e6e8a8e3b2b5e49660adf406dd7ff5d731b957d
Michael Hughes <90157693+mhughes-dev@users.noreply.github.com>
2023-04-15 19:32:51 -0400

    Cleanup polybar configs

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -12,4 +12,3 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 polybar main --config=~/.config/polybar/config.ini &
 polybar second --config=~/.config/polybar/config.ini &
 
-echo "Polybar launched..."
